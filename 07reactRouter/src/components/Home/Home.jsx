f53faf8c0ce24cc294e266b26ba72dd2f316fd46
devrahul-2508 <devrahul2508@gmail.com>
2024-07-10 18:33:07 +0530

    layout fix

--- 07reactRouter/src/components/Home/Home.jsx
+++ 07reactRouter/src/components/Home/Home.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="mx-auto w-full max-w-7xl">
-      <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
-        <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
-          <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
+      <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16 flex flex-col sm:flex-row">
+        <div className="sm:order-1 sm:w-1/2 flex justify-center sm:justify-start">
+          <img
+            className="w-96 sm:w-100 md:w-1/2 lg:w-3/4"
+            src="https://i.ibb.co/5BCcDYB/Remote2.png"
+            alt="image1"
+          />
+        </div>
+
+        <div className="sm:order-2 relative z-10 max-w-screen-xl px-4 pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+          <div className="max-w-xl sm:mt-1 space-y-8 text-center sm:text-right sm:ml-auto">
             <h2 className="text-4xl font-bold sm:text-5xl">
               Download Now
               <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
             </h2>
-
             <Link
               className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
               to="/"
@@ -30,17 +37,9 @@ export default function Home() {
             </Link>
           </div>
         </div>
-
-        <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-          <img
-            className="w-96"
-            src="https://i.ibb.co/5BCcDYB/Remote2.png"
-            alt="image1"
-          />
-        </div>
       </aside>
 
-      <div className="grid  place-items-center sm:mt-20">
+      <div className="grid place-items-center sm:mt-20">
         <img
           className="sm:w-96 w-48"
           src="https://i.ibb.co/2M7rtLk/Remote1.png"

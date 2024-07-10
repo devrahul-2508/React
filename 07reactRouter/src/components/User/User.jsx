06680bbabe775c268ccb8fcbdb3acad4cbca0cda
devrahul-2508 <devrahul2508@gmail.com>
2024-07-10 11:45:15 +0530

    reactRouter

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function User() {
+    const {userid} = useParams()
+  return (
+    <div className="bg-orange-500 text-white text-3xl text-center py-5" >User:{userid}</div>
+  );
+}
